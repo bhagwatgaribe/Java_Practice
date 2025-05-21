daedd1b7fcaddfd79b0fa443f209941a479022b2
Bhagwat <bhagwat22garibe@gmail.com>
2025-05-21 17:18:39 +0530

    Added two claess in loop statement

---
+++ src/looping_Statement/Do_While_Loop.java
@@ -0,0 +1,13 @@
+package looping_Statement;
+
+public class Do_While_Loop {
+
+	public static void main(String[] args) {
+		int number = 1;
+
+		do {
+			System.out.println(number);
+			number++;
+		} while (number < 10);
+	}
+}
