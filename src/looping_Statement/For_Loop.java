91d3e274a46de406cde3563585e54a9d7bc54298
Bhagwat <bhagwat22garibe@gmail.com>
2025-05-21 17:04:36 +0530

    Added class for loop

---
+++ src/looping_Statement/For_Loop.java
@@ -0,0 +1,11 @@
+package looping_Statement;
+
+public class For_Loop {
+
+	public static void main(String[] args) {
+		int number = 10;
+		for (int i = 0; i < number; i++) {
+			System.out.println(i);
+		}
+	}
+}
