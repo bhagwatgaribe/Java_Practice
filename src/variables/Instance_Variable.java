e311e1ab65e86c18fe4ecda2eaa5cd939322898e
Bhagwat <bhagwat22garibe@gmail.com>
2025-05-27 21:16:33 +0530

    abc

--- src/variables/Instance_Variable.java
+++ src/variables/Instance_Variable.java
@@ -1,21 +1,21 @@
 package variables;
 
 public class Instance_Variable {
-	
-	int a=100;
-	int b=200;
-	
+
+	int a = 100;
+	int b = 200;
+
 	public void m1() {
 		System.out.println(a);
 		System.out.println(b);
 	}
-	
+
 	public static void main(String[] args) {
 		Instance_Variable iv = new Instance_Variable();
-		
+
 		System.out.println(iv.a);
 		System.out.println(iv.b);
-		
+
 		iv.m1();
 	}
 }
