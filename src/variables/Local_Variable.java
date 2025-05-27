fd2f619ba91eb3e6e881c1ac4a434b86be1da1e2
Bhagwat <bhagwat22garibe@gmail.com>
2025-05-27 20:59:08 +0530

    added class of instance variable

--- src/variables/Local_Variable.java
+++ src/variables/Local_Variable.java
@@ -1,18 +1,18 @@
 package variables;
 
 public class Local_Variable {
-	
+
 	public void m1() {
-		int i=10;
+		int i = 10;
 		System.out.println(i);
 	}
-	
+
 	public static void main(String[] args) {
-		
-		int a=10;
-		int b=20;
-		System.out.println(a+b);
-		
+
+		int a = 10;
+		int b = 20;
+		System.out.println(a + b);
+
 		Local_Variable lv = new Local_Variable();
 		lv.m1();
 	}
