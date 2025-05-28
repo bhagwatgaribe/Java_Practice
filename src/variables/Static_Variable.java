6d79478fb59db50d42e65f162bc1ce10ff13e112
Bhagwat <bhagwat22garibe@gmail.com>
2025-05-28 17:43:58 +0530

    Static variable class file addedd

--- src/variables/Static_Variable.java
+++ src/variables/Static_Variable.java
@@ -2,19 +2,24 @@ package variables;
 
 public class Static_Variable {
 
-	static int a = 100;
-	static int b = 200;
+	// Instance Variables
+	int x,y,z;
+	int i=10,j,k;
 
-	public void m1() {
-		System.out.println(Static_Variable.a);
-		System.out.println(Static_Variable.b);
-	}
+	// Static Variables
+	static int a=10,b=20,c;
+	static int l=10,m=20,n=30;
+	static int d, e=100,f;
 
 	public static void main(String[] args) {
-		System.out.println(Static_Variable.a);
-		System.out.println(Static_Variable.b);
-
-		Static_Variable sv = new Static_Variable();
-		sv.m1();
+		
+		Static_Variable st = new Static_Variable();
+		
+		System.out.println(st.x+" "+st.y+" "+st.z);
+		System.out.println(st.i+" "+st.j+" "+st.k);
+		
+		System.out.println(Static_Variable.a+" "+Static_Variable.b+" "+Static_Variable.c);
+		System.out.println(Static_Variable.l+" "+Static_Variable.m+" "+Static_Variable.n);
+		System.out.println(Static_Variable.d+" "+Static_Variable.e+" "+Static_Variable.f);
 	}
 }
